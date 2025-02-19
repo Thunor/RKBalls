5f8dbce9f88f975a4805044d24cc17f996e7ffcc
Eric Freitas <efreitos@gmail.com>
2025-02-19 17:54:16 -0600

    Initial Commit

---
+++ RKBalls/RKBallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RKBallsApp.swift
+//  RKBalls
+//
+//  Created by Eric Freitas on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct RKBallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
